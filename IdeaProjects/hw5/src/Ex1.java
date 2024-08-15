507ccb54637b1c2b6e58cf89295280f5d524c6a3
Nguyen Hieu <85873114+wilfierd@users.noreply.github.com>
2024-08-15 12:57:34 +0700

    Update Ex1

--- IdeaProjects/hw5/src/Ex1.java
+++ IdeaProjects/hw5/src/Ex1.java
@@ -1,85 +1,33 @@
-    public class Ex1 {
-        class Person {
-            private String name;
-            private String address;
-            public Person(String name, String address) {
-                this.name = name;
-                this.address = address;
-            }
-            public String getName() {
-                return name;
-            }
-            public String getAddress() {
-                return address;
-            }
-            public void setAddress(String address) {
-                this.address = address;
-            }
-            @Override
-            public String toString() {
-                return "Person[name=" + name + ",address=" + address + "]";
-            }
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MaxMinIntList extends ArrayList<Integer> {
+    
+    // Method to get the minimum value from the list
+    public Integer min() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("List is empty");
         }
-
-        class Student extends Person {
-            private String program;
-            private int year;
-            private double fee;
-
-            public Student(String name, String address, String program, int year, double fee) {
-                super(name, address);
-                this.program = program;
-                this.year = year;
-                this.fee = fee;
-            }
-            public String getProgram() {
-                return program;
-            }
-            public void setProgram(String program) {
-                this.program = program;
-            }
-            public int getYear() {
-                return year;
-            }
-            public void setYear(int year) {
-                this.year = year;
-            }
-            public double getFee() {
-                return fee;
-            }
-            public void setFee(double fee) {
-                this.fee = fee;
-            }
-            @Override
-            public String toString() {
-                return "Student[" + super.toString() + ", program=" + program + ", year=" + year + ", fee=" + fee + "]";
-            }
-        }
-
-        class Staff extends Person {
-            private String school;
-            private double pay;
-            public Staff(String name, String address, String school, double pay) {
-                super(name, address);
-                this.school = school;
-                this.pay = pay;
-            }
-
-            public String getSchool() {
-                return school;
-            }
-            public void setSchool(String school) {
-                this.school = school;
-            }
-            public double getPay() {
-                return pay;
-            }
-            public void setPay(double pay) {
-                this.pay = pay;
-            }
-            @Override
-            public String toString() {
-                return "Staff[" + super.toString() + ", school=" + school + ", pay=" + pay + "]";
-            }
-        }
+        return Collections.min(this);
     }
+
+    // Method to get the maximum value from the list
+    public Integer max() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+        return Collections.max(this);
+    }
+
+    public static void main(String[] args) {
+        MaxMinIntList list = new MaxMinIntList();
+        list.add(5);
+        list.add(1);
+        list.add(9);
+        list.add(3);
+        
+        System.out.println("List: " + list);
+        System.out.println("Minimum: " + list.min());
+        System.out.println("Maximum: " + list.max());
+    }
+}
